bf3ef51768d8087d0ab3b1e49ac1fc9f28fa6c31
Marcos Rocha <zooinm@hotmail.com>
2026-05-11 18:46:24 -0300

    Aula 11052026

--- resources/views/pages/sinopse.blade.php
+++ resources/views/pages/sinopse.blade.php
@@ -8,23 +8,7 @@
 <body class="bg-gray-900 text-gray-100 font-sans">
 
 <!-- Barra de Navegação -->
-<nav class="bg-gray-800 shadow-lg">
-    <div class="max-w-6xl mx-auto px-4">
-        <div class="flex justify-between">
-            <div class="flex space-x-4">
-                <a href="#" class="flex items-center py-5 px-2 text-gray-100 hover:text-yellow-400">
-                    <span class="font-bold text-lg">🎬 Gremlins</span>
-                </a>
-            </div>
-            <div class="flex items-center space-x-4">
-                <a href="#" class="py-5 px-3 text-gray-300 hover:text-white">Início</a>
-                <a href="#" class="py-5 px-3 text-gray-300 hover:text-white">Sinopse</a>
-                <a href="#" class="py-5 px-3 text-gray-300 hover:text-white">Curiosidades</a>
-                <a href="#" class="py-5 px-3 text-gray-300 hover:text-white">Contato</a>
-            </div>
-        </div>
-    </div>
-</nav>
+@include('menu.menu')
 
 <!-- Cabeçalho -->
 <header class="text-center py-10">

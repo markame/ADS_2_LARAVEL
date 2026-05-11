1baae5c34277f78fdf5d32fa919ec56fac6dd3a2
Marcos Rocha <zooinm@hotmail.com>
2026-05-11 18:47:09 -0300

    Aula 11052026

---
+++ resources/views/menu/menu.blade.php
@@ -0,0 +1,20 @@
+<nav class="bg-gray-800 shadow-lg">
+    <div class="max-w-6xl mx-auto px-4">
+        <div class="flex justify-between">
+            <!-- Logo -->
+            <div class="flex space-x-4">
+                <a href="#" class="flex items-center py-5 px-2 text-gray-100 hover:text-yellow-400">
+                    <span class="font-bold text-lg">🎬 Gremlins</span>
+                </a>
+            </div>
+            <!-- Links -->
+            <div class="flex items-center space-x-4">
+                <a href="{{route('grelin_index')}}" class="py-5 px-3 text-gray-300 hover:text-white">Início</a>
+                <a href="{{route('grelin_sinopse')}}" class="py-5 px-3 text-gray-300 hover:text-white">Sinopse</a>
+                <a href="#" class="py-5 px-3 text-gray-300 hover:text-white">Curiosidades</a>
+                <a href="#" class="py-5 px-3 text-gray-300 hover:text-white">Contato</a>
+                <a href="#" class="py-5 px-3 text-gray-300 hover:text-white">Menu Adm</a>
+            </div>
+        </div>
+    </div>
+</nav>
